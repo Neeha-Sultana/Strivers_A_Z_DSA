4162aa0c46cae04c6bab046db4d33778ce5c5b84
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-04-10 18:24:14 +0530

    Create 1_Maximum_Nesting_Depth.py

---
+++ 5_Strings/Basic_And_Easy/1_Maximum_Nesting_Depth.py
@@ -0,0 +1,12 @@
+def maxDepth(s:str) -> int:
+    # Write your code here.
+    ct=0
+    maxi=0
+    for i in range(len(s)):
+        if s[i]=='(':
+            ct+=1
+            maxi=max(ct,maxi)
+        elif s[i]==')':
+            ct-=1
+
+    return maxi
